4ff91f436571ba185ffcaf314be246cabf9b1e69
Youn Mélois <youn@melois.dev>
2024-11-06 11:32:23 +0100

    add port from env

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const client = new MongoClient(process.env.MONGODB_ADDON_URI);
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(
