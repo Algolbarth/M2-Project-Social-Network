f2ad6b618f71bf1fc1b8c721873ce65ac6797fc7
Algolbarth <paul.poirier@live.fr>
2024-11-08 23:06:25 +0100

    fix profil ui

--- public/user.js
+++ public/user.js
@@ -18,16 +18,17 @@ function show_details() {
         details.innerHTML += "Username: " + info.username + "<br />";
         details.innerHTML += "First name: " + info.firstname + "<br />";
         details.innerHTML += "Last name: " + info.lastname + "<br />";
-
         details.innerHTML += "Birthdate: " + print_date(new Date(info.birthdate)) + "<br /><br />";
-
-        details.innerHTML += "Status: " + info.status + "<br /><br />";
 
         if (info.events.length > 0) {
             details.innerHTML += info.events.length + " Events: <br />";
             for (const event of info.events) {
-                details.innerHTML += "<img class='event' src=" + event.imageUrl + "/>";
-                details.innerHTML += event.title + " " + print_date(new Date(event.date)) + "<br />";
+                let text = "";
+                text += "<div class='event'>";
+                text += "<div><img class='event' src=" + event.imageUrl + "/></div>";
+                text += "<div><b>" + event.title + "</b><br />" + print_date(new Date(event.date)) + "</div>";
+                text += "</div>";
+                details.innerHTML += text;
             }
         }
         else {
