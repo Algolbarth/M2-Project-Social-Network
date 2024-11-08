3de75790a0790e55e1d97c242527bc81b022c5d9
Algolbarth <paul.poirier@live.fr>
2024-11-08 22:37:45 +0100

    profil view

--- public/user.js
+++ public/user.js
@@ -13,6 +13,29 @@ function show_details() {
 
         const info = await response.json();
 
-        details.innerHTML = info.username;
+        details.innerHTML = "";
+        details.innerHTML += "<img class='event' src=" + info.avatarUrl + "/> <br />";
+        details.innerHTML += "Username: " + info.username + "<br />";
+        details.innerHTML += "First name: " + info.firstname + "<br />";
+        details.innerHTML += "Last name: " + info.lastname + "<br />";
+
+        details.innerHTML += "Birthdate: " + print_date(new Date(info.birthdate)) + "<br /><br />";
+
+        details.innerHTML += "Status: " + info.status + "<br /><br />";
+
+        if (info.events.length > 0) {
+            details.innerHTML += info.events.length + " Events: <br />";
+            for (const event of info.events) {
+                details.innerHTML += "<img class='event' src=" + event.imageUrl + "/>";
+                details.innerHTML += event.title + " " + print_date(new Date(event.date)) + "<br />";
+            }
+        }
+        else {
+            details.innerHTML += "No Event";
+        }
     });
 };
+
+function print_date (date) {
+    return (date.getMonth() + 1) + "/" + date.getDay() + "/"+ date.getFullYear();
+}
